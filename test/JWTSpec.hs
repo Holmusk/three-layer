110f111e876a036b9e0088590b62c1a303f7f33e
Veronika Romashkina <vrom911@gmail.com>
2018-05-11 19:11:06 +0800

    Switch to hedgehog (#3)
    * Switch to hedgehog
    
    * Fix UUID generation in tests

--- test/JWTSpec.hs
+++ test/JWTSpec.hs
@@ -1,27 +1,36 @@
 module JWTSpec where
 
-import qualified Data.UUID.Types       as UUID
-import           Lib.Util.JWT
-import           System.Random
-import           Test.Tasty
-import           Test.Tasty.QuickCheck
+import Data.UUID.Types (UUID)
+import Hedgehog (MonadGen, Property, forAll, property, (===))
+import System.Random
+import Test.Tasty
+import Test.Tasty.Hedgehog
+
+import Lib.Util.JWT
+
+import qualified Data.UUID.Types as UUID
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+test_jwtMapEncodeAndDecode :: [TestTree]
+test_jwtMapEncodeAndDecode = return $ testProperty "jwtMapEncodeAndDecode" $
+  property $ do
+    randomId <- forAll genRandId
+    let randomJwtPayload = JWTPayload { jwtUserId = randomId }
+    let encoded = jwtPayloadToMap randomJwtPayload
+    let decoded = jwtPayloadFromMap encoded
+    decoded === Just randomJwtPayload
+
+genRandId :: MonadGen m => m UUID
+genRandId = do
+  a <- genWord32
+  b <- genWord32
+  c <- genWord32
+  d <- genWord32
+  pure $ uuidFromWords (a, b, c, d)
+
+genWord32 :: MonadGen m => m Word32
+genWord32 = Gen.word32 (Range.constantBounded @Word32)
 
 uuidFromWords :: (Word32, Word32, Word32, Word32) -> UUID.UUID
 uuidFromWords (a,b,c,d) = UUID.fromWords a b c d
-
-instance Arbitrary UUID.UUID where
-  arbitrary = uuidFromWords <$> arbitrary
-  shrink = map uuidFromWords . shrink . UUID.toWords
-
-instance Arbitrary JWTPayload where
-  arbitrary = do
-    jwtUserId <- arbitrary
-    return JWTPayload{..}
-
-prop_jwtMapEncodeAndDecode :: JWTPayload -> Bool
-prop_jwtMapEncodeAndDecode jwtPayload =
-  let encoded = jwtPayloadToMap jwtPayload
-      decoded = jwtPayloadFromMap encoded
-      didDecode = isJust decoded
-      isStillTheSame = fromMaybe jwtPayload decoded == jwtPayload
-  in didDecode && isStillTheSame
