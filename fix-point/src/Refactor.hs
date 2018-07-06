39435d34503ce0fe99e43172a9612d88d4273212
Tejas Chandrasekar <tsctejas@gmail.com>
2018-07-06 15:22:34 +0800

    Amend fix-point.cabal file and change all string data types to Text in Refactor.hs

--- fix-point/src/Refactor.hs
+++ fix-point/src/Refactor.hs
@@ -1,36 +1,39 @@
 module Rename where 
 
-changeLine :: String -> String -> String 
-changeLine newMod line = case words line of 
+import qualified Data.Text as T
+import qualified Data.Text.IO as Out
+
+changeLine :: T.Text -> T.Text -> T.Text 
+changeLine newMod line = case T.words line of 
     -- module names 
-    "module" : s : extra     -> unwords ("module": prefix s : extra)
+    "module" : s : extra     -> T.unwords ("module": prefix s : extra)
 
     -- imports which start with Lib
-    "import" : s : extra     -> if take libLen s == "Lib" 
-                                then unwords ("import": prefix s : extra) 
+    "import" : s : extra     -> if T.take libLen s == "Lib" 
+                                then T.unwords ("import": prefix s : extra) 
                                 else line 
 
     -- reexport where reexport module started with Lib
-    "(":"module" : s : extra -> unwords ("(" :"module": prefix s : extra)
-    ",":"module" : s : extra -> unwords ("," :"module": prefix s : extra)
+    "(":"module" : s : extra -> T.unwords ("(" :"module": prefix s : extra)
+    ",":"module" : s : extra -> T.unwords ("," :"module": prefix s : extra)
 
     -- Anything else can't be changed
     _ -> line
   where
-    prefix s = renamePrefix newMod (splitAt libLen s)
+    prefix s = renamePrefix newMod (T.splitAt libLen s)
 
 -- To use and avoid calling length function too many times 
 libLen :: Int
-libLen = length "Lib"
+libLen = 3
 
 -- Function replaces Lib prefix with given 1st argument 
-renamePrefix :: String -> (String, String) -> String
-renamePrefix newMod (_, end) = newMod ++ end 
+renamePrefix :: T.Text -> (T.Text, T.Text) -> T.Text
+renamePrefix newMod (_, end) = T.concat [newMod, end] 
 
 -- Method to test if possible without filepath yet 
-rename :: String -> String -> String
-rename newMod s = unlines [changeLine newMod x | x <- lines s]
+rename :: T.Text -> T.Text -> T.Text
+rename newMod s = T.unlines [changeLine newMod x | x <- T.lines s]
 
 -- Method with filePath
-contentRename :: (String -> String -> String) -> String -> FilePath -> IO ()
-contentRename f newMod file = f newMod <$> readFile file >>= putStrLn
+contentRename :: (T.Text -> T.Text -> T.Text) -> T.Text -> FilePath -> IO ()
+contentRename f newMod file = f newMod <$> Out.readFile file >>= Out.putStrLn
