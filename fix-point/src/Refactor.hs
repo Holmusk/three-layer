917245cd1529f48f87d03aa832774e15bb4d37bb
Tejas Chandrasekar <tsctejas@gmail.com>
2018-07-06 10:31:08 +0800

    Remove duplicate calls to newPref function in Refactor.hs

--- fix-point/src/Refactor.hs
+++ fix-point/src/Refactor.hs
@@ -3,19 +3,21 @@ module Rename where
 changeLine :: String -> String -> String 
 changeLine newMod line = case words line of 
     -- module names 
-    "module" : s : extra     -> unwords ("module": newPref newMod s : extra)
+    "module" : s : extra     -> unwords ("module": prefix s : extra)
 
     -- imports which start with Lib
     "import" : s : extra     -> if take libLen s == "Lib" 
-                                then unwords ("import": newPref newMod s : extra) 
+                                then unwords ("import": prefix s : extra) 
                                 else line 
 
     -- reexport where reexport module started with Lib
-    "(":"module" : s : extra -> unwords ("(" :"module": newPref newMod s : extra)
-    ",":"module" : s : extra -> unwords ("," :"module": newPref newMod s : extra)
+    "(":"module" : s : extra -> unwords ("(" :"module": prefix s : extra)
+    ",":"module" : s : extra -> unwords ("," :"module": prefix s : extra)
 
     -- Anything else can't be changed
     _ -> line
+  where
+    prefix s = renamePrefix newMod (splitAt libLen s)
 
 -- To use and avoid calling length function too many times 
 libLen :: Int
@@ -24,10 +26,6 @@ libLen = length "Lib"
 -- Function replaces Lib prefix with given 1st argument 
 renamePrefix :: String -> (String, String) -> String
 renamePrefix newMod (_, end) = newMod ++ end 
-
--- higher level rename 
-newPref :: String -> String -> String
-newPref newMod s = renamePrefix newMod (splitAt libLen s)
 
 -- Method to test if possible without filepath yet 
 rename :: String -> String -> String
