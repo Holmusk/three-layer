dae11a6fe27a51cbd1ada4019a24b50a6553398b
Tejas Chandrasekar <32821216+TejasSC@users.noreply.github.com>
2018-07-09 17:55:42 +0800

    [#27] Part 1: Implement content refactoring tool (#30)
    * Implement content refactor tool first attempt
    
    * Add Refactor.hs to src and add fix-point.cabal file with appropriate changes
    
    * Amend syntax of lines in Refactor.hs
    
    * Delete Refactor.hs
    
    * Implement requested changes on Refactor.hs
    
    * Amend redundant brackets for testing in Refactor.hs
    
    * Clean up layout of Refactor.hs file and fix-point.cabal
    
    * Amend of Refactor.hs to pass test
    
    * Remove duplicate calls to newPref function in Refactor.hs
    
    * Amend fix-point.cabal file and change all string data types to Text in Refactor.hs
    
    * Fix tests for Circle CI
    
    * Amend import statements and delete unnecessary comments in .cabal file
    
    * Comment test data for renaming function in Rename.hs
    
    * Amend test data comments for renaming function in Rename.hs

---
+++ fix-point/src/Rename.hs
@@ -0,0 +1,53 @@
+module Rename where 
+
+import Data.Text (Text)
+import Data.Semigroup ((<>))
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+-- Tests to copy and paste into stack repl, make sure in src folder
+{-
+import qualified Data.Text.IO as TIO
+:{  
+TIO.putStrLn $ rename "CHANGE" "import Lib.App (AppEnv (..))\n\
+\module Lib where\n\
+\( module Lib )\n\
+\, module Lib\n\
+\import Servant.Server (serve)"
+:}
+-}
+changeLine :: Text -> Text -> Text 
+changeLine newMod line = case T.words line of 
+    -- module names 
+    "module" : s : extra     -> T.unwords ("module": prefix s : extra)
+
+    -- imports which start with Lib
+    "import" : s : extra     -> if T.take libLen s == "Lib" 
+                                then T.unwords ("import": prefix s : extra) 
+                                else line 
+
+    -- reexport where reexport module started with Lib
+    "(":"module" : s : extra -> T.unwords ("(" :"module": prefix s : extra)
+    ",":"module" : s : extra -> T.unwords ("," :"module": prefix s : extra)
+
+    -- Anything else can't be changed
+    _ -> line
+  where
+    prefix s = renamePrefix newMod (T.splitAt libLen s)
+
+-- To use and avoid calling length function too many times 
+libLen :: Int
+libLen = 3
+
+-- Function replaces Lib prefix with given 1st argument 
+renamePrefix :: Text -> (Text, Text) -> Text
+renamePrefix newMod (_, end) = newMod <> end 
+
+-- Method to test if possible without filepath yet 
+rename :: Text -> Text -> Text
+rename newMod s = T.unlines [changeLine newMod x | x <- T.lines s]
+
+-- Method with filePath
+contentRename :: (Text -> Text -> Text) -> Text -> FilePath -> IO ()
+contentRename f newMod file = f newMod <$> TIO.readFile file >>= TIO.putStrLn
