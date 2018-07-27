8e7bdb959acd216ec7047fde9ed75ca6027dde9f
Tejas Chandrasekar <tsctejas@gmail.com>
2018-07-27 11:34:36 +0800

    Create executable in Main.hs first attempt subtask 6/7

--- fix-point/app/Main.hs
+++ fix-point/app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import System.Directory (doesDirectoryExist)
+import System.Environment (getArgs)
+
+import CopyFiles (copyAll)
+import Rename
+
 main :: IO ()
-main = putStr "someFunc"
+main = do
+    args <- getArgs
+    isDirectory <- doesDirectoryExist "three-layer"
+    if isDirectory
+    then copyAll "three-layer" args!!0 args!!1
+    else putStrLn "three-layer directory does not exist"
