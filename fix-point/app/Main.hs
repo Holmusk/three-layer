9d5a5daecc5c96c6c4fb11790aef9a02714c28d7
Tejas Chandrasekar <tsctejas@gmail.com>
2018-07-27 17:42:53 +0800

    Subtask 6/7 argument parser

--- fix-point/app/Main.hs
+++ fix-point/app/Main.hs
@@ -1,15 +1,77 @@
 module Main where
 
+import Data.Semigroup ((<>))
+import Options.Applicative (help, Parser, ParserInfo, info, (<**>), header,
+                            helper, execParser, (<|>), metavar, long, short,
+                            fullDesc, progDesc, argument, str)
 import System.Directory (doesDirectoryExist)
-import System.Environment (getArgs)
 
 import CopyFiles (copyAll)
 import Rename
 
 main :: IO ()
-main = do
-    args <- getArgs
-    isDirectory <- doesDirectoryExist "three-layer"
-    if isDirectory
-    then copyAll "three-layer" args!!0 args!!1
-    else putStrLn "three-layer directory does not exist"
+main = run =<< execParser (args `withInfo` "Create project with given name")
+
+run :: Args -> IO ()
+run (Args input) = do
+    case input of
+        Project  proj           -> copyAll "three-layer" proj proj
+        ProjPref proj pref      -> copyAll "three-layer" proj pref
+        StringFP path           -> copyAll path "NEWNAME" "NEWDIR"
+        Combo    path proj pref -> copyAll path proj pref
+
+type Project    = String
+type Prefix     = String
+type SourcePath = FilePath
+
+data FPInput = Project Project
+             | ProjPref Project Prefix
+             | StringFP SourcePath
+             | Combo SourcePath Project Prefix
+
+data Args = Args FPInput
+
+args :: Parser Args
+args = Args <$> parseFPInput
+
+parseFPInput :: Parser FPInput
+parseFPInput = project <|> projPref <|> stringFP <|> combo
+
+project :: Parser FPInput
+project = Project
+    <$> argument str (metavar "PROJECT NAME" <> help "name of project" )
+
+projPref :: Parser FPInput
+projPref = ProjPref
+    <$> argument str
+        (metavar "PROJECT NAME" <> help "name of project" )
+    <*> argument str
+        (  long "prefix"
+        <> short 'p'
+        <> metavar "PREFIX NAME"
+        <> help "name of prefix" )
+
+stringFP :: Parser FPInput
+stringFP = StringFP <$> argument str
+           (  long "source path"
+           <> short 's'
+           <> metavar "SOURCEPATH"
+           <> help "source path" )
+
+combo :: Parser FPInput
+combo = Combo
+    <$> argument str
+    (  long "source path"  -- This line gave a 'no instance error'... 
+    <> short 's'
+    <> metavar "SOURCEPATH"
+    <> help "source path" )
+    <*> argument str
+        (metavar "PROJECT NAME" <> help "name of project" )
+    <*> argument str
+        (  long "prefix"
+        <> short 'p'
+        <> metavar "PREFIX NAME"
+        <> help "name of prefix" )
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts desc = info (helper <*> opts) $ progDesc desc
