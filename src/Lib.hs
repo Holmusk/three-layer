a1cc124e7df6c49b9faf03ae21328ec1bf76b534
Tejas Chandrasekar <tsctejas@gmail.com>
2018-08-06 13:08:48 +0800

    Moving of import to hackage dependencies section issue #26

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,12 +5,11 @@ module Lib
 
 import Network.Wai.Handler.Warp (run)
 import Servant.Server (serve)
+import System.Remote.Monitoring (forkServerWith)
 
 import Lib.App (AppEnv (..))
 import Lib.Core.Jwt (mkRandomString)
 import Lib.Server (API, server)
-
-import System.Remote.Monitoring (forkServerWith)
 
 import qualified Data.HashMap.Strict as HashMap
 import qualified System.Metrics as Metrics
