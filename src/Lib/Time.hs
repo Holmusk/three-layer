fdc0ee5e7220386c77aaebb7474c55f3493082cb
Tejas Chandrasekar <tsctejas@gmail.com>
2018-08-06 15:27:48 +0800

    Add Date data type to protobuf issue #26

--- src/Lib/Time.hs
+++ src/Lib/Time.hs
@@ -1,8 +1,16 @@
 module Lib.Time
        ( Seconds (..)
+       , datetimeToUtc
+       , utcTimeToDatetime
        , dayInSeconds
        , threadDelay
        ) where
+
+import Data.Time.Calendar (fromGregorian, toGregorian)
+import Data.Time.Clock (UTCTime (..), diffTimeToPicoseconds, secondsToDiffTime)
+
+import Proto.Common (Date (..), Datetime (..), Timestamp (..))
+import Proto.Common_Fields (date, day, month, seconds, timestamp, year)
 
 import qualified Control.Concurrent as C (threadDelay)
 
@@ -10,6 +18,40 @@ import qualified Control.Concurrent as C (threadDelay)
 newtype Seconds = Seconds
     { unSeconds :: Int
     } deriving (Show, Eq)
+
+utcTimeToDatetime :: UTCTime -> Datetime
+utcTimeToDatetime UTCTime{..} =
+  let
+    (dyear, dmon, dday) = toGregorian utctDay
+    datePart = Date
+      { _Date'year = fromIntegral dyear
+      , _Date'month = fromIntegral dmon
+      , _Date'day = fromIntegral dday
+      , _Date'_unknownFields = mempty
+      }
+    secondsInDay = diffTimeToPicoseconds utctDayTime `div` (10 ^ (12 :: Int))
+    timePart = Timestamp
+      { _Timestamp'seconds = fromIntegral secondsInDay
+      , _Timestamp'nanos = 0
+      , _Timestamp'_unknownFields = mempty
+      }
+  in
+    Datetime
+      { _Datetime'date = datePart
+      , _Datetime'timestamp = timePart
+      , _Datetime'_unknownFields = mempty
+      }
+
+datetimeToUtc :: Datetime -> UTCTime
+datetimeToUtc datetime =
+    let dtYear  = toInteger $ datetime ^. date . year
+        dtMonth = fromIntegral $ datetime ^. date . month
+        dtDay   = fromIntegral $ datetime ^. date . day
+        diffTime = secondsToDiffTime $ fromIntegral $ datetime ^. timestamp . seconds
+    in UTCTime
+        { utctDay = fromGregorian dtYear dtMonth dtDay
+        , utctDayTime = diffTime
+        }
 
 -- | Similar to 'C.threadDelay' but receive 'Seconds' instead of 'Int'.
 threadDelay :: MonadIO m => Seconds -> m ()
